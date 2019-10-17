d8491827d64a19d242b0dcb598fe12a12c1c3a64
kzavadskii@dengabank.ru <67nsv625>
2019-10-17 11:43:54 +0300

    master updated packages and removed gcloud logging

--- wallets/shared/logging.py
+++ wallets/shared/logging.py
@@ -1,28 +1,45 @@
 import sys
-from google.cloud import logging as g_logging
 from wallets.settings.config import conf
 import logging
-import os
-import json
+import logging.config
+
 
 logger: logging.Logger = None
 
-
-class WhitespaceRemovingFormatter(logging.Formatter):
-    ws_stub = "\\n"
-
-    def format(self, record):
-        record.msg = record.msg.replace("\n", self.ws_stub)
-        logmsg = super().format(record)
-        return {
-            'msg': logmsg,
-            'args': json.dumps(record.args, default=str) or '',
+LOGFILE = f'wallets[{conf.get("ENV")}].log'
+dictLogConfig = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
         }
-
-
-class TestFilter(logging.Filter):
-    def filter(self, record):
-        return not conf.get('TEST')
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'simple'
+        },
+        'file_handler': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'ERROR',
+            'filename': LOGFILE,
+            'formatter': 'verbose',
+            'maxBytes': 1024 * 1024 * 5,
+            'backupCount': 5,
+        }
+    },
+    'loggers': {
+        f'wallets[{conf.get("ENV")}]': {
+            'handlers': ['console', 'file_handler'],
+            'level': 'DEBUG',
+        },
+    }
+}
 
 
 def init_logger():
@@ -30,28 +47,8 @@ def init_logger():
         init_logger connects to cloud logging if it's avaliable
     """
     global logger
-    logger = logging.getLogger('wallets[{}]'.format(conf.get('ENV')))
-    try:
-        logger.setLevel(conf.get('LOGGING_LEVEL', 'INFO'))
-        formatter = WhitespaceRemovingFormatter(
-            '[%(asctime)s][%(levelname)s][%(module)s]: %(message)s'
-        )
-        if not conf.get('TEST') and not os.environ.get('TEST'):
-            client = g_logging.Client(conf.get('PROJECT_NAME', None))
-            handler = client.get_default_handler()
-            logger.handlers = []
-            logger.addHandler(handler)
-            handler.setFormatter(formatter)
-            handler.labels = {
-                'app': 'wallets',
-                'tier': 'backend',
-                'track': conf.get('TRACK', 'no track'),
-                'env': conf.get('ENV') or '',
-            }
-    except Exception as e:
-        logger.error(f"failed getting cloud logger {e}")
-    flt = TestFilter('test')
-    logger.addFilter(flt)
+    logging.config.dictConfig(dictLogConfig)
+    logger = logging.getLogger(f'wallets[{conf.get("ENV")}]')
     return logger
 
 
