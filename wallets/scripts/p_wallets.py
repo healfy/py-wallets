0898862e295f9eae9271c93b49641f266a86c1da
kzavadskii@dengabank.ru <67nsv625>
2020-02-05 10:32:29 +0300

    master: modified name of params

--- wallets/scripts/p_wallets.py
+++ wallets/scripts/p_wallets.py
@@ -8,7 +8,7 @@ from wallets.common.models import Wallet
 
 def create_wallets():
     wallet_btc = Wallet(
-        address=os.environ.get('BTC_COLD_ADDRESS',
+        address=os.environ.get('EXCHANGER_BTC_ADDRESS',
                                'mtQGkRpBVRDdRBwVkCbtGArdCYmiqkQrB1'),
         external_id=os.environ.get('BTC_ID', 23),
         is_platform=True,
@@ -16,7 +16,7 @@ def create_wallets():
     )
 
     wallet_eth = Wallet(
-        address=os.environ.get('ETH_COLD_ADDRESS',
+        address=os.environ.get('EXCHANGER_ETH_ADDRESS',
                                '0xF6E4709341426Dee13c9e9EaB6e4779b299CE2F7'),
         external_id=os.environ.get('ETH_ID', 24),
         is_platform=True,
